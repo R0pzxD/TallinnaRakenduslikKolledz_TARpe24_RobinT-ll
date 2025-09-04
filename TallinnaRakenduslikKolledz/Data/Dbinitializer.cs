3d3416b4311f24fe68460940b773a12860463f8e
opilane <opilane@HP-CZC321705Z>
2025-09-04 15:01:15 +0300

    esimene

---
+++ TallinnaRakenduslikKolledz/Data/Dbinitializer.cs
@@ -0,0 +1,26 @@
+﻿using TallinnaRakenduslikKolledz.Models;
+
+namespace TallinnaRakenduslikKolledz.Data
+{
+    public class Dbinitializer
+    {
+        public static void Initializer(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+            if(context.Students.Any())
+            {
+                return;
+            }
+
+            var students = new Student[]
+            {
+                new Student
+                {
+                    FirstName = "George",
+                    LastName = "Teemus",
+                    EnrollmentDate = DateTime.Now,
+                }
+            };
+        }
+    }
+}
