c71fdd202aa2143579e910d5b56bbbead256a400
Opilane TTHK <opilane@tthk.ee>
2025-09-18 11:58:19 +0300

    a
    a

--- TallinnaRakenduslikKolledz/Controllers/InstructorsController.cs
+++ TallinnaRakenduslikKolledz/Controllers/InstructorsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TallinnaRakenduslikKolledz.Data;
 using TallinnaRakenduslikKolledz.Models;
@@ -20,6 +21,59 @@ namespace TallinnaRakenduslikKolledz.Controllers
             .Include(i => i.CourseAssignments)
             .ToListAsync();
             return View(vm);
+
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var instructor = new Instructor();
+            instructor.CourseAssignments = new List<CourseAssignment>();
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Instructor instructor, string selectedCourses)
+        {
+            if (selectedCourses == null)
+            {
+                instructor.CourseAssignments = new List<CourseAssignment>();
+                foreach (var course in selectedCourses)
+                {
+                    var courseToAdd = new CourseAssignment
+                    {
+                        InstructorId = instructor.Id,
+                        CourseID = course
+
+                    };
+                    instructor.CourseAssignments.Add(courseToAdd);
+                }
+            }
+            if(ModelState.IsValid)
+            {
+                _context.Add(instructor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+            }
+            PopulateAssignedCourseData(instructor);
+            return View(instructor);
+        }
+        private void PopulateAssignedCourseData(Instructor instructor)
+        {
+            var allcourses = _context.Courses;
+            var instructorCourses = new HashSet<int>(instructor.CourseAssignments.Select(c => c.CourseID));
+            var vm = new List<AssignedCourseData>();
+            foreach (var course in allcourses)
+            {
+                vm.Add(new AssignedCourseData
+                {
+                    CourseID = course.CourseId,
+                    Title = course.Title,
+                    Assigned = instructorCourses.Contains(course.CourseId)
+
+                });
+            }
+            ViewData["Courses"] = vm;
 
         }
     }
