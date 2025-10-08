300c67f315f898a61f8e359980a98b22b791feb9
Opilane TTHK <opilane@tthk.ee>
2025-10-08 11:27:22 +0300

    Delinquents base model valmis

---
+++ TallinnaRakenduslikKolledz/Models/DelinquentsBase.cs
@@ -0,0 +1,15 @@
+﻿namespace TallinnaRakenduslikKolledz.Models
+{
+    public class DelinquentsBase
+    {
+      public string FirstName { get; set; }
+      public string LastName { get; set; }
+      public enum Violaton
+      {
+         Smoking,Gooning,Sleeping,Drugs,Alcohol
+      }
+        public bool? isTeacherOrStudent { get; set; }
+
+        public string ViolationDescription { get; set; }
+    }
+}
